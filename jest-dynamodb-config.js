173a568691fa7e2e797ce3069453be36af6f4f72
Hardys <hardyscc@gmail.com>
2020-04-20 20:11:29 +0800

    fix: test tables creation missmatch

--- jest-dynamodb-config.js
+++ jest-dynamodb-config.js
@@ -1,26 +1,26 @@
 module.exports = async () => {
-  // const serverless = new (require('serverless'))();
+  const serverless = new (require('serverless'))();
 
-  // await serverless.init();
-  // const service = await serverless.variables.populateService();
+  await serverless.init();
+  const service = await serverless.variables.populateService();
 
-  // const extractTableResources = (r) =>
-  //   Object.keys(r)
-  //     .map((name) => r[name])
-  //     .filter((r) => r.Type === 'AWS::DynamoDB::Table')
-  //     .map((r) => r.Properties);
+  const extractTableResources = (r) =>
+    Object.keys(r)
+      .map((name) => r[name])
+      .filter((r) => r.Type === 'AWS::DynamoDB::Table')
+      .map((r) => r.Properties);
 
-  // const tables = [];
-  // if (Array.isArray(service.resources)) {
-  //   service.resources.map((r) => {
-  //     tables.push(...extractTableResources(r.Resources));
-  //   });
-  // } else {
-  //   tables.push(...extractTableResources(service.resources.Resources));
-  // }
+  const tables = [];
+  if (Array.isArray(service.resources)) {
+    service.resources.map((r) => {
+      tables.push(...extractTableResources(r.Resources));
+    });
+  } else {
+    tables.push(...extractTableResources(service.resources.Resources));
+  }
 
   return {
-    tables: [],
+    tables,
     port: 8001,
     options: ['-inMemory'],
     installerConfig: {
