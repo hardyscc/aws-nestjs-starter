b96e797b462994a11a08cfe4988d556b9b23beb4
Hardys <hardyscc@users.noreply.github.com>
2021-10-28 15:30:21 +0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -9,19 +9,26 @@ import { AppModule } from './app.module';
 let cachedServer: Handler;
 
 const bootstrapServer = async (): Promise<Handler> => {
-	const expressApp = express();
-	const app = await NestFactory.create(AppModule, new ExpressAdapter(expressApp));
-	app.useGlobalPipes(new ValidationPipe({ forbidUnknownValues: true }));
-	app.enableCors();
-	await app.init();
-	return serverlessExpress({
-		app: expressApp
-	});
+  const expressApp = express();
+  const app = await NestFactory.create(
+    AppModule,
+    new ExpressAdapter(expressApp),
+  );
+  app.useGlobalPipes(new ValidationPipe({ forbidUnknownValues: true }));
+  app.enableCors();
+  await app.init();
+  return serverlessExpress({
+    app: expressApp,
+  });
 };
 
-export const handler: APIGatewayProxyHandler = async (event, context, callback) => {
-	if (!cachedServer) {
-		cachedServer = await bootstrapServer();
-	}
-	return cachedServer(event, context, callback);
+export const handler: APIGatewayProxyHandler = async (
+  event,
+  context,
+  callback,
+) => {
+  if (!cachedServer) {
+    cachedServer = await bootstrapServer();
+  }
+  return cachedServer(event, context, callback);
 };
